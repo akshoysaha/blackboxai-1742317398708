552d2c769fe23ec70b9d8362f8b16b8d2ac3650e
Akshoy Saha <akshoysaha@gmail.com>
2025-03-19 13:10:52 +0530

    Add talke/app/entry.client.tsx 1742370051478

--- talke/app/entry.client.tsx
+++ talke/app/entry.client.tsx
@@ -1,18 +1,33 @@
-/**
- * By default, Remix will handle hydrating your app on the client for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.client
- */
+import { useState } from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { CacheProvider } from '@emotion/react';
+import { RemixBrowser } from '@remix-run/react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import ClientStyleContext from './styles/client.context';
+import createEmotionCache from './styles/createEmotionCache';
 
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+interface ClientCacheProviderProps {
+  children: React.ReactNode;
+}
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = useState(createEmotionCache());
+
+  function reset() {
+    setCache(createEmotionCache());
+  }
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
   );
-});
+}
+
+hydrateRoot(
+  document,
+  <ClientCacheProvider>
+    <RemixBrowser />
+  </ClientCacheProvider>
+);
