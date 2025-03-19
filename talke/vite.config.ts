22cf82e38d5e8d84c0ad3cb1ed19cd05be73dd14
Akshoy Saha <akshoysaha@gmail.com>
2025-03-19 13:11:35 +0530

    Add talke/vite.config.ts 1742370094464

--- talke/vite.config.ts
+++ talke/vite.config.ts
@@ -2,23 +2,28 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
   plugins: [
     remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
+      ignoredRouteFiles: ["**/.*"],
     }),
     tsconfigPaths(),
   ],
+  server: {
+    port: 3000,
+    host: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'matrix-js-sdk': ['matrix-js-sdk'],
+          'mui': ['@mui/material', '@mui/icons-material'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['matrix-js-sdk'],
+  },
 });
